b6a7d231be51ec9646129a14863d0cea6e40daba
juthalen <judith.alende@gmail.com>
2023-05-29 19:19:32 +0200

    Cambios en clase

--- models/models.py
+++ models/models.py
@@ -7,7 +7,7 @@ import re
 from odoo.exceptions import ValidationError
 
 class modulo_clase(models.Model):
-    _name = 'modulo.modulo'
+    _name = 'modulo.clase'
     _description = 'clases'
 
     name = fields.Char(string="Nombre", required=True)
@@ -23,6 +23,22 @@ class modulo_clase(models.Model):
         ('6',"Formación y Orientación Laboral")],
         string="Clases"
     )
+    #Relaciones con profesor
+    profesor_id = fields.Many2one(
+        comodel_name="modulo.profesor",
+        string="Profesor",
+        required=True
+    )
+    #Relaciones con alumno
+    alumno_ids = fields.Many2many(
+        comodel_name="modulo.alumno",
+        relation="clase_alumno_rel",
+        column1="clase_id",
+        column2="alumno_id",
+        string="Alumnos",
+        ondelete="set null"
+    )
+
 
     @api.constrains('hora_inicio', 'hora_fin')
     def _check_hora(self):
@@ -49,4 +65,95 @@ class modulo_profesor(models.Model):
     name = fields.Char(string="Nombre", required=True)
     apellidos = fields.Char(string="Apellidos")
     fecha_nacimiento = fields.Date(string="Fecha de nacimiento")
+    email = fields.Char(string="Email")
+    telefono = fields.Char(string="Teléfono")
+
+    #Relaciones con clases
+    clase_ids = fields.One2many(
+        comodel_name="modulo.clase", 
+        inverse_name="profesor_id",
+        string="Clases",
+        ondelete="set null")
+    
+    #Relaciones con alumno
+    alumno_ids = fields.Many2many(
+        comodel_name="modulo.alumno",
+        relation="clase_alumno_rel",
+        column1="alumno_id",
+        column2="clase_id",
+        string="Alumnos",
+        ondelete="set null"
+    )
+    
+    #constrain
+    @api.constrains('fecha_nacimiento')
+    def _check_fecha_nacimiento(self):
+        for record in self:
+            if record.fecha_nacimiento > datetime.now().strftime("%Y-%m-%d"):
+                raise ValidationError("La fecha de nacimiento debe ser anterior a la fecha actual")
+
+    @api.constrains('email')
+    def _check_email(self):
+        for record in self:
+            if not re.match(r'[^@]+@[^@]+\.[^@]+', record.email):
+                raise ValidationError("El email debe ser válido")
+    
+    @api.constrains('telefono')
+    def _check_telefono(self):
+        for record in self:
+            if not re.match(r'^\d{9}$', record.telefono):
+                raise ValidationError("El teléfono debe ser válido")
+
+class modulo_alumno(models.Model):
+    _name = "modulo.alumno"
+    _description = "Alumno"
+
+    name = fields.Char(string="Nombre", required=True)
+    apellidos = fields.Char(string="Apellidos")
+    fecha_nacimiento = fields.Date(string="Fecha de nacimiento")
+    email = fields.Char(string="Email")
+    dni = fields.Char(string="DNI")
+    telefono = fields.Char(string="Teléfono")
+    #Relaciones con clases
+    clase_ids = fields.Many2many(
+        comodel_name="modulo.clase",
+        relation="clase_alumno_rel",
+        column1="clase_id",
+        column2="alumno_id",
+        string="Clases",
+        ondelete="set null")
+    #Relaciones con profesor
+    profesor_id = fields.Many2one(
+        comodel_name="modulo.profesor",
+        string="Profesor",
+        required=True
+    )
+    
+    #constrain
+    @api.constrains('fecha_nacimiento')
+    def _check_fecha_nacimiento(self):
+        for record in self:
+            if record.fecha_nacimiento > datetime.now().strftime("%Y-%m-%d"):
+                raise ValidationError("La fecha de nacimiento debe ser anterior a la fecha actual")
+    
+    @api.constrains('email')
+    def _check_email(self):
+        for record in self:
+            if not re.match(r'[^@]+@[^@]+\.[^@]+', record.email):
+                raise ValidationError("El email debe ser válido")
+
+    @api.constrains('dni')
+    def _check_dni(self):
+        for record in self:
+            if not re.match(r'^\d{8}[a-zA-Z]$', record.dni):
+                raise ValidationError("El DNI debe ser válido")
+    
+    @api.constrains('telefono')
+    def _check_telefono(self):
+        for record in self:
+            if not re.match(r'^\d{9}$', record.telefono):
+                raise ValidationError("El teléfono debe ser válido")
+    
+  
+
     
